6922f28dd06f010edb83dcd807c6c2196740b9c6
Donyaee-Aliasghar <donyaeealiasghar@gmail.com>
2025-08-10 23:57:56 +0330

    #4 (add, update) add sample fasta file(data) and small changing in comment pathes

--- Fasta_files_analysis/src/fast_files_analysis/pathes.py
+++ Fasta_files_analysis/src/fast_files_analysis/pathes.py
@@ -2,17 +2,17 @@
 
 from pathlib import Path
 
-# current python file path.
+# Current python file path.
 CURRENT_FILE = Path(__file__).resolve()
 
-#! this paths is in my system(please comment this part and use below codes).
-# get raw datas folder.
+#! This paths is in my system(please comment this part and use below codes).
+# Get raw datas folder.
 MAIN_RAW_DATAS_FOLDER = CURRENT_FILE.parent.parent.parent.parent.parent.parent
 MAIN_RAW_FILE = MAIN_RAW_DATAS_FOLDER / "Raw_datas/Bioinformatics/FASTA/Homo_sapiens.GRCh38.dna.chromosome.1.fa"
 
-#! this paths is you must used.
-# # get root parent folder.
+#! This paths is you must used.
+# # Get root parent folder.
 # parent_folder = CURRENT_FILE.parent.parent.parent  # go 2 folder back
 
-# # get main file.
+# # Get main file.
 # RAW_DATA = parent_folder / "datas/fasta.fa"
