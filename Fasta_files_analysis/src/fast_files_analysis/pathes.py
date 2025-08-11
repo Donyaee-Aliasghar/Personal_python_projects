29a2a7c09d28f4ddfcb9cffaaa09672330ee2bf4
Donyaee-Aliasghar <donyaeealiasghar@gmail.com>
2025-08-11 09:51:26 +0330

    #6 (update). Correcting spelling mistakes.

--- Fasta_files_analysis/src/fast_files_analysis/pathes.py
+++ Fasta_files_analysis/src/fast_files_analysis/pathes.py
@@ -23,5 +23,5 @@ ROOT_PATH = CURRENT_FILE.parent.parent.parent  # go 3 folder back.
 OUTPUT_DIR = Path(f"{ROOT_PATH}/results/outputs")
 OUTPUT_DIR.mkdir(exist_ok=True)
 
-LOG_DIR = Path(f"{ROOT_PATH}/results/log")
+LOG_DIR = Path(f"{ROOT_PATH}/results/logs")
 LOG_DIR.mkdir(exist_ok=True)
